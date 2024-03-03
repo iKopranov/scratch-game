0ed71caede6717ac1b8fd949b7c5d5735f0a2592
Kopranych <kopranych@gmail.com>
2024-03-03 15:32:25 +0500

    cyberspeed: add javadoc

--- src/main/java/com/cyberspeed/scratchgame/service/engine/IGameEngine.java
+++ src/main/java/com/cyberspeed/scratchgame/service/engine/IGameEngine.java
@@ -1,15 +1,53 @@
 package com.cyberspeed.scratchgame.service.engine;
 
+/**
+ * This interface defines the contract for a generic game engine.
+ *
+ * @param <T> The type of configuration used to initialize the game.
+ * @param <K> The type representing winning combinations.
+ * @param <M> The type representing the game matrix.
+ * @param <B> The type representing bonuses in the game.
+ */
 public interface IGameEngine<T, K, M, B> {
-  
+
+  /**
+   * Initializes the game engine with the provided configuration.
+   *
+   * @param config The configuration object used to initialize the game.
+   */
   void initGame(T config);
 
+  /**
+   * Retrieves the game matrix.
+   *
+   * @return The game matrix.
+   */
   M getMatrix();
 
+  /**
+   * Determines the winning combinations based on the provided game matrix.
+   *
+   * @param matrix The game matrix.
+   * @return The winning combinations.
+   */
   K getWinCombinations(M matrix);
 
+  /**
+   * Calculates the reward amount based on the bet amount, winning combinations,
+   * and bonus in the game.
+   *
+   * @param betAmount      The amount bet by the player.
+   * @param winCombinations The winning combinations in the game.
+   * @param bonus          The bonus applied in the game.
+   * @return The reward amount.
+   */
   Double getReward(Double betAmount, K winCombinations, B bonus);
 
-
+  /**
+   * Retrieves the bonus based on the provided game matrix.
+   *
+   * @param matrix The game matrix.
+   * @return The bonus applied in the game.
+   */
   B getBonus(M matrix);
 }
