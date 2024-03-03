dbe2233550f9f05d7c90fee7f9e72d1afbede763
Kopranych <kopranych@gmail.com>
2024-03-03 18:51:03 +0500

    cyberspeed: implement same symbols horizontally

--- src/main/java/com/cyberspeed/scratchgame/service/engine/impl/AdvancedGameEngine.java
+++ src/main/java/com/cyberspeed/scratchgame/service/engine/impl/AdvancedGameEngine.java
@@ -3,6 +3,7 @@ package com.cyberspeed.scratchgame.service.engine.impl;
 import com.cyberspeed.scratchgame.model.GameConfig;
 import com.cyberspeed.scratchgame.model.WinCombination;
 import com.cyberspeed.scratchgame.service.ProbabilityService;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +11,7 @@ import java.util.Map;
 
 public class AdvancedGameEngine extends SimpleGameEngine {
   
+  private static final String SAME_SYMBOL_HORIZONTALLY = "same_symbols_horizontally";
   
   public AdvancedGameEngine(GameConfig gameConfig, ProbabilityService<String> probabilityService) {
     super(gameConfig, probabilityService);
@@ -26,7 +28,37 @@ public class AdvancedGameEngine extends SimpleGameEngine {
   public Map<String, List<WinCombination>> getWinCombinations(List<List<String>> matrix) {
     var symbolWinCombinations = new HashMap<String, List<WinCombination>>();
     super.addSameSymbolWinCombinations(matrix, symbolWinCombinations);
-    //todo: add 
+    addSameSymbolsHorizontallyWinCombinations(matrix, symbolWinCombinations);
     return symbolWinCombinations;
+  }
+
+  public void addSameSymbolsHorizontallyWinCombinations(
+      List<List<String>> matrix, HashMap<String, List<WinCombination>> symbolWinCombinations
+  ) {
+    var winCombination = super.getGameConfig().getWinCombinations().get(SAME_SYMBOL_HORIZONTALLY);
+    var rowsSize = super.getGameConfig().getRows();
+
+    for (var row : matrix) {
+      String symbol = null;
+      int count = 0;
+      for (var cell : row) {
+        if (symbol == null || cell.equals(symbol)) {
+          count++;
+        } else {
+          addWinCombination(symbol, count, rowsSize, winCombination, symbolWinCombinations);
+          count = 1;
+        }
+        symbol = cell;
+      }
+      addWinCombination(symbol, count, rowsSize, winCombination, symbolWinCombinations);
+    }
+  }
+
+  private void addWinCombination(
+      String symbol, int count, int rows, WinCombination winCombination,
+      HashMap<String, List<WinCombination>> symbolWinCombinations) {
+    if (count >= rows) {
+      symbolWinCombinations.computeIfAbsent(symbol, k -> new ArrayList<>()).add(winCombination);
+    }
   }
 }
--- src/test/java/com/cyberspeed/scratchgame/service/engine/impl/AdvancedGameEngineTest.java
+++ src/test/java/com/cyberspeed/scratchgame/service/engine/impl/AdvancedGameEngineTest.java
@@ -3,12 +3,14 @@ package com.cyberspeed.scratchgame.service.engine.impl;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.cyberspeed.scratchgame.model.GameConfig;
+import com.cyberspeed.scratchgame.model.WinCombination;
 import com.cyberspeed.scratchgame.service.impl.ProbabilityServiceImpl;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -29,17 +31,74 @@ class AdvancedGameEngineTest {
   @Test
   void shouldReturnMatrix() {
     // given
-    final var simpleGameEngine =
-        new AdvancedGameEngine(gameConfig, new ProbabilityServiceImpl(gameConfig.getProbabilities()));
+    final var gameEngine =
+        new AdvancedGameEngine(gameConfig,
+            new ProbabilityServiceImpl(gameConfig.getProbabilities()));
 
     // when
-    final var matrix = simpleGameEngine.getMatrix();
+    final var matrix = gameEngine.getMatrix();
 
     // then
     assertEquals(4, matrix.size());
     for (List<String> row : matrix) {
       assertEquals(4, row.size());
     }
+  }
+
+  @Test
+  void shouldReturnSameSymbolWinCombinations() {
+    // given
+    final var gameEngine =
+        new AdvancedGameEngine(gameConfig,
+            new ProbabilityServiceImpl(gameConfig.getProbabilities()));
+    var matrix = List.of(
+        List.of("A", "A", "B", "F"),
+        List.of("A", "+1000", "B", "C"),
+        List.of("A", "A", "B", "D")
+    );
+
+    // when
+    final var winCombinations = gameEngine.getWinCombinations(matrix);
+
+    // then
+    var expectedWinCombinations = Map.of("A",
+        List.of(new WinCombination("same_symbol_5_times", 2d, "same_symbols", 5L, "same_symbols",
+            null)),
+        "B",
+        List.of(
+            new WinCombination("same_symbol_3_times", 1d, "same_symbols", 3L, "same_symbols", null))
+    );
+    assertEquals(expectedWinCombinations, winCombinations);
+  }
+
+  @Test
+  void shouldReturnHorizontallyLinearWinCombinations() {
+    // given
+    final var gameEngine =
+        new AdvancedGameEngine(gameConfig,
+            new ProbabilityServiceImpl(gameConfig.getProbabilities()));
+    var matrix = List.of(
+        List.of("F", "A", "B", "F"),
+        List.of("A", "+1000", "B", "C"),
+        List.of("D", "D", "D", "D")
+    );
+
+    // when
+    final var winCombinations = gameEngine.getWinCombinations(matrix);
+
+    // then
+    var expectedWinCombinations = Map.of("D",
+        List.of(new WinCombination("same_symbol_4_times", 1.5, "same_symbols", 4L, "same_symbols",
+                null),
+            new WinCombination("same_symbols_horizontally", 2d, "linear_symbols", null,
+                "horizontally_linear_symbols", List.of(
+                List.of("0:0", "0:1", "0:2"),
+                List.of("1:0", "1:1", "1:1"),
+                List.of("2:0", "2:1", "2:2")
+            ))
+        ));
+    
+    assertEquals(expectedWinCombinations, winCombinations);
   }
 
 }
